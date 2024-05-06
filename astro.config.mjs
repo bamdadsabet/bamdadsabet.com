33f59d39cb619a86843906a2ff9bf063c37735f2
bamdadsabet <bamdadsabet@gmail.com>
2024-05-06 22:18:42 +0330

    chore: Global css added to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "./src/assets/styles/main.scss";',
+        },
+      },
+    },
+  },
+  integrations: [tailwind(), react()]
+});
