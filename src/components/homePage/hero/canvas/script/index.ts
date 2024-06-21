5a392f489cd7592100a06b35d5f6aa76f3d34cca
bamdadsabet <bamdadsabet@gmail.com>
2024-06-21 14:07:54 +0330

    chore: Added comment to improve readability

--- src/components/homePage/hero/canvas/script/index.ts
+++ src/components/homePage/hero/canvas/script/index.ts
@@ -1,5 +1,6 @@
 import renderCanvas from "./renderCanvas";
 
+// initializing canvas
 const canvas: HTMLCanvasElement | null =
 document.querySelector("canvas#canvas");
 canvas && renderCanvas(canvas);
