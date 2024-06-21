5e79267897224acc17eeee3f39f6d63729b43741
bamdadsabet <bamdadsabet@gmail.com>
2024-06-21 14:11:27 +0330

    chore: Added comment to improve readability

--- src/components/homePage/hero/typewriter/script/index.ts
+++ src/components/homePage/hero/typewriter/script/index.ts
@@ -1,10 +1,15 @@
 import typeWriterEffect from "./typewriterEffect";
 
+// getting typewriter html element 
 const typeWriter = document.querySelector(
   "div.type-writer"
 ) as HTMLElement;
 
+// getting typewriter parameters 
 const titles = typeWriter.getAttribute("data-titles")?.split(",");
 const typingElement = typeWriter.querySelector("span");
 
-if (typingElement && titles) typeWriterEffect(titles, typingElement);
+// initializing typewriter
+if (typingElement && titles) {
+  typeWriterEffect(titles, typingElement);
+}
